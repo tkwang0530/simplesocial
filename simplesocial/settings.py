09a0b96b4b62615c84f3a20d541193d2ea84a2d2
Tommy <tkwang0530@gmail.com>
2021-08-23 21:40:13 +0800

    update general setting

--- simplesocial/settings.py
+++ simplesocial/settings.py
@@ -40,7 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_bootstrap5',
-    'accounts'
+    'accounts',
+    'groups',
+    'posts'
 ]
 
 MIDDLEWARE = [
@@ -122,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_FIR = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 LOGIN_REDIRECT_URL = 'test'
 LOGOUT_REDIRECT_URL = 'thanks'
